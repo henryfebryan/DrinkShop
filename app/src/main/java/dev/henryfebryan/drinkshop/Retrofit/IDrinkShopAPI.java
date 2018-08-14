ed5ae4a2fe0f3694863ee57fbc1b9ab45a58adf5
henryfebryan <henry_febryan@yahoo.com>
2018-08-14 18:54:36 +0700

    Initial commit

---
+++ app/src/main/java/dev/henryfebryan/drinkshop/Retrofit/IDrinkShopAPI.java
@@ -0,0 +1,22 @@
+package dev.henryfebryan.drinkshop.Retrofit;
+
+import dev.henryfebryan.drinkshop.Model.CheckUserResponse;
+import dev.henryfebryan.drinkshop.Model.User;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface IDrinkShopAPI {
+    @FormUrlEncoded
+    @POST("checkuser.php")
+    Call<CheckUserResponse> checkUserExists(@Field("phone") String phone);
+
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<User> registerNewUser(@Field("phone") String phone,
+                               @Field("name") String name,
+                               @Field("address") String address,
+                               @Field("birthdate") String birthdate);
+
+}
---
+++ app/src/main/java/dev/henryfebryan/drinkshop/Utils/Common.java
@@ -0,0 +1,13 @@
+package dev.henryfebryan.drinkshop.Utils;
+
+import dev.henryfebryan.drinkshop.Retrofit.IDrinkShopAPI;
+import dev.henryfebryan.drinkshop.Retrofit.RetrofitClient;
+
+public class Common {
+    //in Emulator , localhost = 10.0.2.2
+    private static final String BASE_URL = "http://10.0.2.2/drinkshop/";
+
+    public static IDrinkShopAPI getAPI(){
+        return RetrofitClient.getClient(BASE_URL).create(IDrinkShopAPI.class);
+    }
+}
